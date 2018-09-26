49246acbb4252f564149f337f963457be46115d9
tmoe <timolang@gmail.com>
2018-09-26 13:33:11 +0200

    Fixed example for basic transcoding

--- manual_decode_overlay_encode.c
+++ manual_decode_overlay_encode.c
@@ -40,6 +40,9 @@ static FILE *dest_file;
 static struct CONTEXT_T {
     VCOS_SEMAPHORE_T semaphore;
     MMAL_QUEUE_T *queue_encoded;
+    MMAL_PORT_T* encoder_input_port;
+    MMAL_PORT_T* encoder_output_port;
+    MMAL_POOL_T * encoder_pool_in;
     MMAL_STATUS_T status;
 } context;
 
@@ -58,6 +61,39 @@ static void log_video_format(MMAL_ES_FORMAT_T *format)
 }
 
 
+/** Callback from the encoder input port.
+ * Buffer has been consumed and is available to be used again. */
+static void encoder_input_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
+{
+    struct CONTEXT_T *ctx = (struct CONTEXT_T *)port->userdata;
+
+    /* The encoder is done with the data, just recycle the buffer header into its pool */
+    mmal_buffer_header_release(buffer);
+
+    /* Kick the processing thread */
+   // vcos_semaphore_post(&ctx->semaphore);
+
+    //fprintf(stderr,"encoder input callback\n");
+}
+
+
+/** Callback from the encoder output port.
+ * Buffer has been produced by the port and is available for processing. */
+static void encoder_output_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
+{
+    struct CONTEXT_T *ctx = (struct CONTEXT_T *)port->userdata;
+
+    /* Queue the encoded video frame so that we can grab it in the main loop*/
+    mmal_queue_put(ctx->queue_encoded, buffer);
+
+    /* Kick the processing thread */
+    vcos_semaphore_post(&ctx->semaphore);
+
+    //fprintf(stderr,"encoder output callback\n");
+}
+
+
+
 /** Callback from the decoder input port.
  * Buffer has been consumed and is available to be used again. */
 static void decoder_input_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
@@ -74,20 +110,87 @@ static void decoder_input_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buff
 }
 
 
-/** Callback from the encoder output port.
+
+/** Callback from the pool. Buffer is available. */
+static MMAL_BOOL_T pool_buffer_available_callback(MMAL_POOL_T *pool, MMAL_BUFFER_HEADER_T *buffer,
+   void *userdata)
+{
+   MMAL_PARAM_UNUSED(userdata);
+
+   mmal_queue_put(pool->queue, buffer);
+
+   return 0;
+}
+
+
+/** Callback from the decoder output port.
  * Buffer has been produced by the port and is available for processing. */
-static void encoder_output_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
+static void decoder_output_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer)
 {
     struct CONTEXT_T *ctx = (struct CONTEXT_T *)port->userdata;
 
-    /* Queue the encoded video frame */
-    mmal_queue_put(ctx->queue_encoded, buffer);
+    if (buffer->cmd) {
+        if (buffer->cmd != MMAL_EVENT_FORMAT_CHANGED) {
+            fprintf(stderr,"unknown cmd: %u\n",buffer->cmd);
+            return;
+        }
+
+        MMAL_EVENT_FORMAT_CHANGED_T *event = mmal_event_format_changed_get(buffer);
+        if (!event){
+            fprintf(stderr,"could not get format change event");
+            return;
+        }
+
+        MMAL_STATUS_T status = mmal_format_full_copy(ctx->encoder_input_port->format, event->format);
+        if (status != MMAL_SUCCESS) {
+            fprintf(stderr,"could not copy format from event to input format: %s",mmal_status_to_string(status));
+            return;
+        }
+        status = mmal_port_format_commit(ctx->encoder_input_port);
+        if (status != MMAL_SUCCESS) {
+          fprintf(stderr,"could not commit input format: %s",mmal_status_to_string(status));
+          return;
+        }
+
+        status = mmal_port_enable(ctx->encoder_input_port, encoder_input_callback);
+        if (status != MMAL_SUCCESS) {
+          fprintf(stderr,"could not enable encoder input port: %s",mmal_status_to_string(status));
+          return;
+        }
+
+        status = mmal_port_enable(ctx->encoder_output_port, encoder_output_callback);
+        if (status != MMAL_SUCCESS) {
+          fprintf(stderr,"could not enable encoder output port: %s",mmal_status_to_string(status));
+          return;
+        }
+
+        //create pool with corret buffer requirements
+        ctx->encoder_pool_in =mmal_port_pool_create(ctx->encoder_input_port,ctx->encoder_input_port->buffer_num_recommended,ctx->encoder_input_port->buffer_size_recommended);
+        mmal_pool_callback_set(ctx->encoder_pool_in, pool_buffer_available_callback, NULL);
+
+        mmal_buffer_header_release(buffer);
+        fprintf(stderr,"Encoder enabled\n");
+
+    } else {
+
+        ctx->status = mmal_port_send_buffer(ctx->encoder_input_port, buffer);
+        if (ctx->status != MMAL_SUCCESS)
+        {
+            fprintf(stderr,"could not send buffer from decoder output to encoder input: %s\n",
+                    mmal_status_to_string(ctx->status));
+            mmal_buffer_header_release(buffer);
+            //mmal_event_error_send(connection->out->component, status);
+        }
+    }
+
 
     /* Kick the processing thread */
-    vcos_semaphore_post(&ctx->semaphore);
+    //vcos_semaphore_post(&ctx->semaphore);
 
-    //fprintf(stderr,"encoder output callback\n");
+    //fprintf(stderr,"decoder output callback\n");
 }
+
+
 
 
 /** Callback from the control port.
@@ -112,16 +215,17 @@ static void decoder_control_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *bu
     /* Done with the event, recycle it */
     mmal_buffer_header_release(buffer);
 
-    fprintf(stderr,"control cb. status %u\n", ctx->status);
+    fprintf(stderr,"control cb. status %s\n",mmal_status_to_string(ctx->status));
 }
+
 
 int main(int argc, char* argv[]) {
 
     MMAL_STATUS_T status;
-    MMAL_CONNECTION_T *conn = NULL;
+    //MMAL_CONNECTION_T *conn = NULL;
     MMAL_COMPONENT_T *decoder = NULL, *encoder=NULL;
     MMAL_POOL_T *decoder_pool_in = NULL, *encoder_pool_out = NULL;
-    MMAL_ES_FORMAT_T * format_in=NULL, *format_out=NULL;
+    MMAL_ES_FORMAT_T * format_in=NULL ;
     MMAL_BOOL_T eos_sent = MMAL_FALSE, eos_received;
     MMAL_BUFFER_HEADER_T *buffer;
     int framenr=0;
@@ -176,11 +280,9 @@ int main(int argc, char* argv[]) {
     status = mmal_port_format_commit(decoder->input[0]);
     CHECK_STATUS(status, "failed to commit format");
 
-
-    format_out = decoder->output[0]->format;
-
-    status = mmal_port_format_commit(decoder->output[0]);
-    CHECK_STATUS(status, "failed to commit format");
+    //format_out = decoder->output[0]->format;
+    //status = mmal_port_format_commit(decoder->output[0]);
+    //CHECK_STATUS(status, "failed to commit format");
 
     decoder->input[0]->buffer_num = decoder->input[0]->buffer_num_min;
     decoder->input[0]->buffer_size = decoder->input[0]->buffer_size_min;
@@ -189,37 +291,32 @@ int main(int argc, char* argv[]) {
     decoder->output[0]->buffer_size = decoder->output[0]->buffer_size_min;
 
     decoder_pool_in = mmal_port_pool_create(decoder->input[0],decoder->input[0]->buffer_num,decoder->input[0]->buffer_size);
+    encoder_pool_out = mmal_port_pool_create(encoder->output[0], encoder->output[0]->buffer_num, encoder->output[0]->buffer_size);
+
 
     context.queue_encoded = mmal_queue_create();
+    context.encoder_pool_in = NULL; //pool cannot be allocated yet, because buffer requirements are not known yet. See decoder output callback
+    context.encoder_input_port = encoder->input[0];
+    context.encoder_output_port = encoder->output[0];
 
     /* Store a reference to our context in each port (will be used during callbacks) */
-    decoder->input[0]->userdata = (struct MMAL_PORT_USERDATA_T*)(void *)&context;
-    encoder->output[0]->userdata =  (struct MMAL_PORT_USERDATA_T*)(void *)&context;
+    decoder->input[0]->userdata = (void *)&context;
+    decoder->output[0]->userdata = (void*) &context;
+    encoder->input[0]->userdata = (void*) &context;
+    encoder->output[0]->userdata = (void *)&context;
 
     status = mmal_port_enable(decoder->input[0], decoder_input_callback);
-    CHECK_STATUS(status, "failed to enable input port")
+    CHECK_STATUS(status, "failed to enable decoder input port")
+
+    status = mmal_port_enable(decoder->output[0], decoder_output_callback);
+    CHECK_STATUS(status, "failed to enable decoder output port")
 
 
-
-    /* connect them up - this propagates port settings from outputs to inputs */
-    status = mmal_connection_create(&conn, decoder->output[0], encoder->input[0], 0);
-    log_video_format(encoder->input[0]->format);
-    CHECK_STATUS(status, "failed to connect decoder to encoder")
+    //we do not enable the encoder ports yet. Instead we wait on the format change event on the decoder output. Then we finish configuring the encoder.
 
 
-    /* Start playback */
+    /* Start transcoding */
     fprintf(stderr, "start transcoding\n");
-    status = mmal_connection_enable(conn);
-    CHECK_STATUS(status, "failed to enable connection");
-
-    status = mmal_port_enable(encoder->output[0], encoder_output_callback);
-    CHECK_STATUS(status, "failed to enable output port");
-
-    encoder_pool_out = mmal_port_pool_create(encoder->output[0], encoder->output[0]->buffer_num, encoder->output[0]->buffer_size);
-    while((buffer = mmal_queue_get(encoder_pool_out->queue)) != NULL)
-    {
-        mmal_port_send_buffer(encoder->output[0], buffer);
-    }
 
     while(eos_received == MMAL_FALSE)
     {
@@ -227,7 +324,7 @@ int main(int argc, char* argv[]) {
         vcos_semaphore_wait(&context.semaphore);
 
         /* Send data to decode to the input port of the video decoder */
-        if ((buffer = mmal_queue_get(decoder_pool_in->queue)) != NULL)
+        if ((buffer = mmal_queue_get(decoder_pool_in->queue)) != NULL) //Get empty buffers from queue
         {
             SOURCE_READ_DATA_INTO_BUFFER(buffer);
             if(!buffer->length) eos_sent = MMAL_TRUE;
@@ -261,11 +358,21 @@ int main(int argc, char* argv[]) {
             mmal_buffer_header_release(buffer);
         }
 
-        /* Send empty buffers to the output port of the encoder */
-        while ((buffer = mmal_queue_get(encoder_pool_out->queue)) != NULL)
-        {
-            status = mmal_port_send_buffer(encoder->output[0], buffer);
-            CHECK_STATUS(status, "failed to send buffer");
+        if(encoder->output[0]->is_enabled) { //do not send buffers until all ports and pools are created
+
+            /* Send empty buffers to the output port of the decoder */
+            while((buffer = mmal_queue_get(context.encoder_pool_in->queue)) != NULL)
+            {
+                status =  mmal_port_send_buffer(decoder->output[0], buffer);
+                CHECK_STATUS(status, "failed to send empty buffer to decoder output\n");
+            }
+
+            /* Send empty buffers to the output port of the encoder */
+            while ((buffer = mmal_queue_get(encoder_pool_out->queue)) != NULL)
+            {
+                status = mmal_port_send_buffer(encoder->output[0], buffer);
+                CHECK_STATUS(status, "failed to send empty buffer to encoder output\n");
+            }
         }
 
 
@@ -276,12 +383,12 @@ int main(int argc, char* argv[]) {
 
     /* Stop everything. Not strictly necessary since mmal_component_destroy()
        * will do that anyway */
+
     mmal_port_disable(decoder->input[0]);
     mmal_port_disable(decoder->control);
+    mmal_port_disable(encoder->input[0]);
+    mmal_port_disable(decoder->output[0]);
     mmal_port_disable(encoder->output[0]);
-
-    /* Stop everything */
-    mmal_connection_disable(conn);
     fprintf(stderr, "done\n");
 
     SOURCE_CLOSE();
@@ -289,8 +396,6 @@ int main(int argc, char* argv[]) {
 
 error:
     /* Cleanup everything */
-    if (conn)
-        mmal_connection_destroy(conn);
     if (decoder)
         mmal_component_release(decoder);
     if (encoder)
